7e64526e6799341ced13d2459571539be11ed370
Fatma <fatmahussein355@gmail.com>
2025-07-08 19:01:11 +0300

    my portfolio

---
+++ script.js
@@ -0,0 +1,14 @@
+// script.js
+const fadeIns = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    }
+  });
+}, {
+  threshold: 0.1
+});
+
+fadeIns.forEach(el => observer.observe(el));
